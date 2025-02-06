9452dd7d579620dda82afd2237e786db32cbb14c
Ryan Pierce <66081922+rpierce521@users.noreply.github.com>
2025-02-06 12:31:16 -0500

    Add type hint files and initial test files for NumPy and PyArrow packages

--- assets/Projects/about_me_chatbot/flask_app/app.py
+++ assets/Projects/about_me_chatbot/flask_app/app.py
@@ -1,15 +1,16 @@
+
+
 from flask import Flask, request, jsonify
-import openai
+from openai import OpenAI
 import os
 from flask_cors import CORS
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"), 
+)
 
 @app.route('/api/chat', methods=['POST'])
 def chat():
